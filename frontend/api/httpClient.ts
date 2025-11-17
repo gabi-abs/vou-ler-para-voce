dfe637e249eb7114b76d7bf063503c2ce0e3997e
Gabriella Santana <gabriellasantana2201@gmail.com>
2025-11-17 00:43:54 -0300

    httpClient

--- frontend/api/httpClient.ts
+++ frontend/api/httpClient.ts
@@ -2,11 +2,7 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
 export const httpClient = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://192.168.0.5:9090/",
-=======
   baseURL: process.env.EXPO_PUBLIC_API_URL || "http://localhost:8080/",
->>>>>>> 02e112c57ce3f1b0e1a5909e5d717534f2c17702
 });
 
 httpClient.interceptors.request.use(async config => {
