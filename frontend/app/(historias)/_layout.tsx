66181d38c1f0ddd1d3f6fe689f51dcf207cb154f
Gabriella Santana <gabriellasantana2201@gmail.com>
2025-11-24 23:03:13 -0300

    Mudando cor do header com base na tela

--- frontend/app/(historias)/_layout.tsx
+++ frontend/app/(historias)/_layout.tsx
@@ -1,9 +1,35 @@
 // app/(historias)/historias/_layout.tsx
 import Favoritas from "@/assets/svg/favoritas.svg";
 import Historias from "@/assets/svg/historias.svg";
-import { Tabs } from "expo-router";
+import { theme } from "@/themes";
+import { Tabs, useNavigation, usePathname } from "expo-router";
+import { useEffect } from "react";
 
 export default function HistoriasTabsLayout() {
+  const navigation = useNavigation();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (navigation == null) return;
+
+    const isFavoritas = pathname.endsWith("/favoritas");
+
+    if (isFavoritas) {
+      navigation.setOptions({
+        headerStyle: {
+          backgroundColor: "#F9ECFD",
+        },
+      });
+      return;
+    }
+
+    navigation.setOptions({
+      headerStyle: {
+        backgroundColor: theme.colors.background,
+      },
+    });
+  }, [pathname]);
+
   return (
     <Tabs>
       <Tabs.Screen
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -5,6 +5,8 @@ import 'react-native-reanimated';
 
 import { AuthProvider } from '@/context/AuthContext';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { theme } from '@/themes';
+import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export const unstable_settings = {
@@ -15,21 +17,42 @@ const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  const screenOptions: NativeStackNavigationOptions  = {
+      headerShown: true,
+      contentStyle: {
+        backgroundColor: theme.colors.background,
+      },
+      statusBarTranslucent: true,
+      presentation: "card", 
+      headerBackTitle: "Voltar", 
+      headerTitle: "",
+      headerStyle: {
+        backgroundColor: theme.colors.background,
+      },
+      headerShadowVisible: false,
+  }
   
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
+      <StatusBar
+        style="auto"
+        translucent
+        backgroundColor="transparent"
+      />
+      <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          <Stack screenOptions={{presentation: "card", headerBackTitle: "Voltar", headerTitle: ""}}>
+          <Stack screenOptions={screenOptions}>
             <Stack.Screen name="index" options={{ presentation: 'modal', headerShown: false }} />
-            <Stack.Screen name="(historias)" />
+            <Stack.Screen name="(historias)"  />
             <Stack.Screen name="gravar/[historiaId]" options={{ headerTitle: 'Histórias' }} />
             <Stack.Screen name="editar/[historiaId]" options={{ headerTitle: 'Histórias' }} />
             <Stack.Screen name="ouvir/[historiaId]" options={{ headerTitle: 'Histórias'}} />
           </Stack>
-          <StatusBar style="auto" />
         </ThemeProvider>
       </AuthProvider>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </>
   );
 }
